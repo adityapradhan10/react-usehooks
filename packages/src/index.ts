1eb022ec0666070804fce87366964b1d12420739
utsavpatel51 <utsavpatel51@gmail.com>
2024-01-21 22:58:47 +0530

    Added set 1 hooks

--- packages/src/index.ts
+++ packages/src/index.ts
@@ -1,1 +1,6 @@
-export * from './useBoolean/useBoolean';
+export * from "./useBoolean/useBoolean";
+export * from "./useDebounce/useDebounce";
+export * from "./useLocalStorage/useLocalStorage";
+export * from "./useOnline/useOnline";
+export * from "./useSessionStorage/useSessionStorage";
+export * from "./useStorage/useStorage";
--- packages/src/useBoolean/useBoolean.ts
+++ packages/src/useBoolean/useBoolean.ts
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import { FunctionVoid } from '../utils';
+import { useCallback, useState } from "react";
+import { FunctionVoid } from "../utils";
 
 interface useBooleanOutput {
-  value: boolean;
-  setTrue: FunctionVoid;
-  setFalse: FunctionVoid;
-  toggle: FunctionVoid;
+	value: boolean;
+	setTrue: FunctionVoid;
+	setFalse: FunctionVoid;
+	toggle: FunctionVoid;
 }
 
 export function useBoolean(defaultValue?: boolean): useBooleanOutput {
-  const [value, setValue] = useState(!!defaultValue);
+	const [value, setValue] = useState(!!defaultValue);
 
-  const setTrue = () => setValue(true);
-  const setFalse = () => setValue(false);
-  const toggle = () => setValue((value) => !value);
+	const setTrue = useCallback(() => setValue(true), []);
+	const setFalse = useCallback(() => setValue(false), []);
+	const toggle = useCallback(() => setValue((value) => !value), []);
 
-  return { value, setTrue, setFalse, toggle };
+	return { value, setTrue, setFalse, toggle };
 }
